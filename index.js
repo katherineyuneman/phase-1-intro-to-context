f1eabd1a2e595cf1604dc5b2f0fe5c6f3edbb0e8
KatherineYuneman <kyuneman@gmail.com>
2021-11-26 12:15:15 -0500

    commit tree 0f25a9983101eb4fc861e03c8f65f7cabd235772

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-// Your code here
+function createEmployeeRecord(array){
+  // [fNameInput, lNameInput, titleInput, hourlyPayInput]
+  let employeeObject = {
+    firstName: array[0],
+    familyName: array[1],
+    title: array[2],
+    payPerHour: array[3],
+    timeInEvents: [],
+    timeOutEvents: [],
+  }
+  return employeeObject;
+}
+
+createEmployeeRecord();
+
+function createEmployeeRecords(employeeObject){
+  let employeeObjectArray = Object.entries(employeeObject)
+  return employeeObjectArray
+}
